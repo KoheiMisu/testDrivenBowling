2bee55377ed0fa54d2561a83c22f016d7700f57e
kohei <parkour12019@gmail.com>
2016-09-28 21:37:13 +0900

    SpareCase

--- app/BowlingGame.php
+++ app/BowlingGame.php
@@ -4,12 +4,53 @@ namespace App;
 
 class BowlingGame
 {
+    /** @var int **/
+    private $score;
+
+    /** @var int **/
+    private $lastPin;
+
+    /** @var bool **/
+    private $isSpare;
+
+    /** @var int **/
+    private $shotNo;
+
+    public function __construct()
+    {
+        $this->score = 0;
+        $this->isSpare = false;
+        $this->lastPin = 0;
+    }
+
     /**
      * @param  int    $pin [description]
      * @return [type]      [description]
      */
     public function recordShot(int $pin)
     {
+        ++$this->shotNo;
+
+        $this->score += $pin;
+
+        /**
+         * スペアの場合、もう一度ピンの数を足して
+         * フラグを下げる
+         */
+        if ($this->isSpare) {
+            $this->score += $pin;
+            $this->isSpare = false;
+        }
+
+        /**
+         * 2投目のときにスペアかどうか判定
+         * 10点とったらフラグを立てる
+         */
+        if ($this->shotNo === 2 && $pin + $this->lastPin === 10) {
+            $this->isSpare = true;
+        }
+
+        $this->lastPin = $pin;
     }
 
     /**
@@ -17,6 +58,6 @@ class BowlingGame
      */
     public function calculateScore()
     {
-        return 0;
+        return $this->score;
     }
 }
--- tests/BowlingGameTest.php
+++ tests/BowlingGameTest.php
@@ -22,12 +22,78 @@ class BowlingGameTest extends TestCase
      */
     public function testAllGarter()
     {
-        $this->BowlingGame = new BowlingGame();
-
-        for ($i = 0; $i < 20; $i++) {
-            $this->BowlingGame->recordShot(0);
-        }
+        $this->loopRecordShot(20, 0);
 
         $this->assertTrue(0 === $this->BowlingGame->calculateScore());
+    }
+
+    /**
+     * 全投球で 1 ピンのみ倒す
+     */
+    public function testKnockedDownAllOne()
+    {
+        $this->loopRecordShot(20, 1);
+
+        $this->assertEquals(20, $this->BowlingGame->calculateScore());
+    }
+
+    /**
+     * スペア時の計算
+     *
+     * 1投目: 3
+     * 2投目: 7  //スペア
+     * 3投目: 4
+     *
+     * 4投目以降、ガター
+     *
+     *
+     * @return [type] [description]
+     */
+    public function testSpareCase()
+    {
+        $this->BowlingGame->recordShot(3);
+        $this->BowlingGame->recordShot(7);
+        /** ↑ スペア **/
+
+        $this->BowlingGame->recordShot(4);
+        $this->loopRecordShot(17, 0);
+
+        $this->assertEquals(18, $this->BowlingGame->calculateScore());
+    }
+
+    /**
+     * 直前の投球との合計が10だった場合に
+     * スペアではないと判断しているか
+     *
+     * 1投目: 2
+     * 2投目: 5
+     * 3投目: 5 //スペア ??
+     * 3投目: 8
+     *
+     * @return [type] [description]
+     */
+    public function testSpareCaseAtDifferentFlame()
+    {
+        $this->BowlingGame->recordShot(2);
+        $this->BowlingGame->recordShot(5);
+        $this->BowlingGame->recordShot(5);
+        $this->BowlingGame->recordShot(8);
+
+        $this->loopRecordShot(16, 0);
+
+        $this->assertEquals(20, $this->BowlingGame->calculateScore());
+    }
+
+
+    /**
+　　  * @param  int    $loop [description]
+　　  * @param  int    $pin  [description]
+　　  * @return void       [description]
+　　  */
+    private function loopRecordShot(int $loop, int $pin)
+    {
+        for ($i = 0; $i < $loop; $i++) {
+            $this->BowlingGame->recordShot($pin);
+        }
     }
 }
