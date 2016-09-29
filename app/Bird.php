6152a4d0c30c4688d397d9f9b32644c391bbc2a5
kohei <parkour12019@gmail.com>
2016-09-30 00:09:09 +0900

    Add Hp property

--- app/Bird.php
+++ app/Bird.php
@@ -4,8 +4,24 @@ namespace App;
 
 class Bird
 {
+    /** int **/
+    private $hp;
+
+    public function __construct()
+    {
+        $this->hp = 10;
+    }
+
     public function fly()
     {
+        --$this->hp;
+
         return 'flyAway';
     }
+
+    public function showHp()
+    {
+        return $this->hp;
+    }
+
 }
--- tests/BirdTest.php
+++ tests/BirdTest.php
@@ -8,10 +8,27 @@ class BowlingGameTest extends TestCase
     /** @var Bird **/
     public $Bird;
 
-    public function testFly()
+    public function setUp()
     {
         $this->Bird = new Bird();
+    }
+
+    public function tearDown()
+    {
+        unset($this->Bird);
+    }
+
+    public function testFly()
+    {
         $this->assertTrue('flyAway' === $this->Bird->fly());
+    }
+
+    public function testHp()
+    {
+        $this->Bird->fly();
+        $this->Bird->fly();
+
+        $this->assertEquals(8, $this->Bird->showHp());
     }
 
 }
